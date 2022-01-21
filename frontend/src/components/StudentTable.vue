ad6f9f6cbab9946cfe886e25e890c25950bb5a51
julianapsilva <julianadepaula001@gmail.com>
2022-01-21 17:59:08 -0300

    inicia tela de detalhes

---
+++ frontend/src/components/StudentTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="table-wrapper">
+    <div class="table-details">
+      <p>Relatórios enviados</p>
+      <button class="primary">Submeter relatório</button>
+    </div>
+    <table>
+      <tr>
+        <th>Número</th>
+        <th>Data de envio</th>
+        <th>Data de entrega</th>
+        <th>Situação</th>
+        <th class="without-padding">Ações</th>
+      </tr>
+      <tr>
+        <td>001</td>
+        <td>02/05/2020</td>
+        <td>03/03/2021</td>
+        <td>Regular</td>
+        <td class="without-padding"><button>Visualizar</button></td>
+      </tr>
+      <tr>
+        <td>001</td>
+        <td>02/05/2020</td>
+        <td>03/03/2021</td>
+        <td>Regular</td>
+        <td class="without-padding"><button>Visualizar</button></td>
+      </tr>
+      <tr>
+        <td>001</td>
+        <td>02/05/2020</td>
+        <td>03/03/2021</td>
+        <td>Regular</td>
+        <td class="without-padding"><button>Visualizar</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StudentTable',
+  props: {},
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+.table-wrapper {
+  width: 1160px;
+  margin-left: 220px;
+  margin-top: 40px;
+}
+.table-wrapper td,
+th {
+  padding-right: 195px;
+  padding-top: 10px;
+}
+table {
+  background-color: #bdbfc1;
+  padding: 20px;
+  padding-top: 10px;
+}
+
+.table-details {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+</style>
---
+++ frontend/src/views/student/InternDetails.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <Header />
+    <div>
+      <div class="details">
+        <div class="situation">
+          <h1>Estágio na empresa {{ companyName }}</h1>
+          <p><b>Situação: </b>Ativo</p>
+        </div>
+        <div class="buttons-section">
+          <button class="secondary">Pedir renovação</button>
+          <button class="secondary">Alterar dados</button>
+          <button class="secondary">Encerrar estágio</button>
+        </div>
+      </div>
+      <StudentTable />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import StudentTable from '@/components/StudentTable.vue'
+
+export default {
+  name: 'InternDetails',
+  props: {},
+  data () {
+    return {
+      companyName: 'UFJF'
+    }
+  },
+  components: { Header, StudentTable },
+  methods: {},
+  created () {}
+}
+</script>
+
+<style>
+.details {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  max-width: 80%;
+  margin-left: 70px;
+}
+
+.situation {
+  text-align: left;
+}
+
+.buttons-section button {
+  margin-left: 15px;
+}
+</style>
