94e4e8a3865eb17ad58aedf4ba621b2777a720cf
julianapsilva <julianadepaula001@gmail.com>
2022-01-21 17:22:45 -0300

    configura eslint e prettier

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/essential', 
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    'generator-star-spacing': 'off',
+  }
+}
