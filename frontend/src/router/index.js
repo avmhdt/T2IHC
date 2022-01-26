9982539c195d5662551ab5c0464acefacd0683aa
julianapsilva <julianadepaula001@gmail.com>
2022-01-26 19:21:50 -0300

    adiciona path login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/student/Home.vue'
 import InternDetails from '@/views/student/InternDetails.vue'
+import Login from '@/views/shared/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/details',
     name: 'InternDetails',
     component: InternDetails
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
