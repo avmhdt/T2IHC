e631d157addf0a131dcbcf3a9552c9b8804438bc
Condé, Deborah <deborahsc23@gmail.com>
2022-01-28 08:24:59 -0300

    cadastrar estagio

--- backend/API_IHC/Repositories/InMemEstagioRepository.cs
+++ backend/API_IHC/Repositories/InMemEstagioRepository.cs
@@ -7,15 +7,15 @@ namespace API_IHC.Repositories
     public class InMemEstagioRepository
     {
         private readonly List<Estagio> estagios;
-      
+
         public InMemEstagioRepository()
         {
-            var aluno1 = new Aluno(); aluno1.Nome = "Mario"; aluno1.Matricula = "2020";  aluno1.Curso = "Engenharia Mecânica"; aluno1.TipoUsuario = Usuario.Aluno;
+            var aluno1 = new Aluno(); aluno1.Nome = "Mario"; aluno1.Matricula = "2020"; aluno1.Curso = "Engenharia Mecânica"; aluno1.TipoUsuario = Usuario.Aluno;
             var aluno2 = new Aluno(); aluno2.Nome = "Lara Croft"; aluno2.Matricula = "1996"; aluno2.Curso = "Historia"; aluno2.TipoUsuario = Usuario.Aluno;
 
             this.estagios = new List<Estagio>();
             estagios.Add(new Estagio() { Id = 001, Empresa = "Nintendo", DataInicio = DateTime.Now, Aluno = aluno1 });
-            estagios.Add(new Estagio() { Id = 002, Empresa = "Sony", DataInicio = DateTime.Now, Aluno = aluno2 }); 
+            estagios.Add(new Estagio() { Id = 002, Empresa = "Sony", DataInicio = DateTime.Now, Aluno = aluno2 });
         }
 
         public IEnumerable<Estagio> GetEstagios()
@@ -28,13 +28,15 @@ namespace API_IHC.Repositories
             return estagios.Where(estagio => estagio.Id == id).SingleOrDefault();
         }
 
-        public Estagio CadastrarEstagio(Estagio estagio)
+        public bool CadastrarEstagio(Estagio estagio)
         {
-            if (estagio == null)
-                throw new ArgumentNullException("estagio");
+            if (estagio != null)
+            {
+                estagios.Add(estagio);
+                return true;
+            }
             else
-                estagio = new Estagio();
+                return false;
         }
-
     }
 }
