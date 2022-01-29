91c8293de0075ca57f5c33808022adba08066325
julianapsilva <julianadepaula001@gmail.com>
2022-01-29 17:43:37 -0300

    configura VUEX

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import store from './store'
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
@@ -13,5 +14,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: (h) => h(App)
 }).$mount('#app')
---
+++ frontend/src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    name: '',
+    typeofUser: 0
+  },
+  mutations: {
+    SET_USER (state, payload) {
+      state.name = payload.usuario
+      state.typeofUser = payload.tipoUsuario
+      console.log(state)
+    }
+  },
+  actions: {},
+  modules: {}
+})
