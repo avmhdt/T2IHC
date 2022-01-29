7f80a415bf6dcad9c23bd9ae684242be7af35027
julianapsilva <julianadepaula001@gmail.com>
2022-01-29 16:40:46 -0300

    inicia sidebar

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: {},
+  data () {
+    return {}
+  },
+  components: {},
+  methods: {},
+  created () {}
+}
+</script>
+
+<style></style>
