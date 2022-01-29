411176436c2e9fb49c6d06f028940a124075393f
julianapsilva <julianadepaula001@gmail.com>
2022-01-29 11:49:30 -0300

    rota coordenador

---
+++ frontend/src/views/coordinator/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Home',
+  props: {},
+  data () {
+    return {}
+  },
+  components: { Header },
+  methods: {},
+  created () {}
+}
+</script>
+
+<style></style>
