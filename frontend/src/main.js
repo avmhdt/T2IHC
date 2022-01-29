8685f9a7bb08e7944027668cf5d9c8454988e94f
julianapsilva <julianadepaula001@gmail.com>
2022-01-29 15:32:45 -0300

    instala fontawesome e moment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,17 @@
+import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
