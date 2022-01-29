c5e1e2cd53109231c71520a18207556c1cbb404a
julianapsilva <julianadepaula001@gmail.com>
2022-01-29 15:33:52 -0300

    inicio da tela de home para coordenador

---
+++ frontend/src/components/InternsTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>Nome do aluno</th>
+        <th>Nome da empresa</th>
+        <th>Data de início</th>
+        <th>Data de fim</th>
+        <th class="without-padding">Ações</th>
+      </tr>
+
+      <tr v-for="(item, index) in interns" :key="item.id">
+        <td>Aluno {{ index + 1 }}</td>
+        <td>{{ item.empresa }}</td>
+        <td>{{ format_date(item.dataInicio) }}</td>
+        <td>{{ format_date(item.dataFim) }}</td>
+        <td class="without-padding"><button>Visualizar</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'InternsTable',
+  props: {
+    interns: Array
+  },
+  data () {
+    return {}
+  },
+  components: {},
+  methods: {
+    format_date (value) {
+      if (value) {
+        return moment(String(value)).format('DD/MM/YYYY')
+      }
+    }
+  },
+  created () {}
+}
+</script>
+
+<style></style>
--- frontend/src/views/coordinator/Home.vue
+++ frontend/src/views/coordinator/Home.vue
@@ -1,22 +1,71 @@
 <template>
   <div>
     <Header />
+    <h1>Olá, {{ name }}!</h1>
+    <div class="search-input">
+      <font-awesome-icon icon="search" />
+      <input
+        type="text"
+        name="search"
+        placeholder="Procurar por aluno..."
+        v-model="search"
+        @keyup.enter="searchByName"
+      />
+    </div>
+    <InternsTable :interns="estagios" />
   </div>
 </template>
 
 <script>
+import api from '@/services/api.js'
 import Header from '@/components/Header.vue'
+import InternsTable from '@/components/InternsTable.vue'
 
 export default {
   name: 'Home',
   props: {},
   data () {
-    return {}
+    return {
+      name: 'Lorenza',
+      search: '',
+      estagios: []
+    }
   },
-  components: { Header },
+  components: { Header, InternsTable },
   methods: {},
-  created () {}
+  async created () {
+    const { data } = await api.get('/api/Estagio/getEstagios')
+    this.estagios = data
+  }
 }
 </script>
 
-<style></style>
+<style>
+.search-input input {
+  height: 30px;
+  width: 100%;
+  padding: 10px 60px;
+  box-shadow: 1px 4px 4px 0px rgba(0, 0, 0, 0.03);
+  border: 0.5px solid rgba(0, 0, 0, 0.04);
+  border-radius: 15px;
+  text-align: left;
+  outline: none;
+  background: #c4c4c4;
+}
+.search-input {
+  position: relative;
+  width: 50%;
+}
+
+.search-input input::placeholder {
+  color: #000;
+}
+
+.search-input svg {
+  position: absolute;
+  left: 0;
+  margin-top: 15px;
+  margin-left: 20px;
+  width: 20px;
+}
+</style>
