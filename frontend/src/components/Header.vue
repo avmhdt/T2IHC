9f9313050ff177f95c47ddc0bb03f19873f89344
julianapsilva <julianadepaula001@gmail.com>
2022-01-30 17:07:44 -0300

    rota condicional para usuario

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
-  <router-link to="/">
+  <router-link v-if="$store.state.typeofUser" to="/">
     <img src="@/assets/logo.png" alt="Logo da UFJF" class="logo" />
   </router-link>
+  <router-link v-else to="/Home/Coord">
+    <img src="@/assets/logo.png" alt="Logo da UFJF" class="logo" />
+  </router-link>
+
 </template>
 
 <script>
